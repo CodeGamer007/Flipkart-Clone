e48ed8bc07a8fc8fec761bb8bb3f93ce50b938dd
CodeGamer007 <100952792+CodeGamer007@users.noreply.github.com>
2023-01-04 16:48:09 +0530

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,7 @@
+function login() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active-blur');
+
+    let form = document.getElementById('form');
+    form.classList.toggle('active-blur');
+}
---
+++ js/signup.js
@@ -0,0 +1,7 @@
+function signup() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active-blur');
+
+    let form = document.getElementById('signup-form');
+    form.classList.toggle('active-blur');
+}
